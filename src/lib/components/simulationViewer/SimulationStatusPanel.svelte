e0dffa750068c5d52f47599f3540b8bc3928db42
wooningeire <22846982+wooningeire@users.noreply.github.com>
2025-11-25 01:07:30 -0500

    refactor: move status panel to own component

---
+++ src/lib/components/simulationViewer/SimulationStatusPanel.svelte
@@ -0,0 +1,103 @@
+<script lang="ts">
+import type { SimulationState } from "./SimulationState.svelte";
+import ElapsedTimeDisplay from "./ElapsedTimeDisplay.svelte";
+import Separator from "$lib/components/generic/Separator.svelte";
+import Button from "$lib/components/generic/Button.svelte";
+import Hotkey from "$lib/components/headless/Hotkey.svelte";
+import { GpuRenderMethodType } from "$lib/gpu/GpuRenderMethod";
+
+let {
+    simulationState,
+    status,
+    err,
+}: {
+    simulationState: SimulationState,
+    status: string,
+    err: string | null,
+} = $props();
+</script>
+
+<simulation-status-panel>
+    <h3>Status</h3>
+    <div>{err !== null ? `error: ${err}` : status}</div>
+
+    <Separator />
+
+    <h3>Render method</h3>
+
+    <div>
+        <input
+            type="radio"
+            name="render-method-type"
+            bind:group={simulationState.renderMethodType}
+            value={GpuRenderMethodType.Points}
+            id="render-method-type_points"
+        />
+        <label for="render-method-type_points">Points</label>
+    </div>
+
+    <div>
+        <input
+            type="radio"
+            name="render-method-type"
+            bind:group={simulationState.renderMethodType}
+            value={GpuRenderMethodType.Raymarch}
+            id="render-method-type_raymarch"
+        />
+        <label for="render-method-type_raymarch">Raymarch</label>
+    </div>
+
+    <Separator />
+
+    <h3>Elapsed time</h3>
+
+    <dl>
+        <dt>Time spent in GPU (sample)</dt>
+        <dd>
+            <ElapsedTimeDisplay
+                ns={simulationState.elapsedTime.gpuTimeNs}
+                inverseLabel="commands / s"
+            />
+        </dd>
+
+        <dt>Total animation frame time</dt>
+        <dd>
+            <ElapsedTimeDisplay
+                ns={simulationState.elapsedTime.animationFrameTimeNs}
+                showMsFractionalPart={false}
+            />
+        </dd>
+    </dl>
+
+    <Separator />
+
+    <h3>Simulation controls</h3>
+
+    <Hotkey
+        key="r"
+        onKeyUp={() => simulationState.reset()}
+    >
+        {#snippet pressTarget({keyHeld})}
+            <Button
+                {keyHeld}
+                onclick={() => simulationState.reset()}
+            >Reset (R)</Button>
+        {/snippet}
+    </Hotkey>
+</simulation-status-panel>
+
+<style lang="scss">
+simulation-status-panel {
+    width: 20rem;
+    padding: 0.5rem;
+
+    line-height: 1.25;
+
+    color: oklch(1 0 0);
+}
+
+h3 {
+    font-size: 1.5rem;
+    margin-bottom: 1rem;
+}
+</style>
--- src/lib/components/simulationViewer/SimulationViewer.svelte
+++ src/lib/components/simulationViewer/SimulationViewer.svelte
@@ -1,12 +1,8 @@
 <script lang="ts">
 import Canvas from "./Canvas.svelte";
-import Separator from "$lib/components/generic/Separator.svelte";
-import { GpuRenderMethodType } from "$lib/gpu/GpuRenderMethod";
-import ElapsedTimeDisplay from "./ElapsedTimeDisplay.svelte";
 import { SimulationState } from "./SimulationState.svelte";
 import { onMount } from "svelte";
-import Button from "$lib/components/generic/Button.svelte";
-import Hotkey from "$lib/components/headless/Hotkey.svelte";
+import SimulationStatusPanel from "./SimulationStatusPanel.svelte";
 
 let status = $state("loading javascript");
 let err = $state<string | null>(null);
@@ -32,74 +28,11 @@ onMount(() => {
         bind:canvas
     />
 
-    <overlays-panel>
-        <h3>Status</h3>
-        <div>{err !== null ? `error: ${err}` : status}</div>
-
-        <Separator />
-
-        <h3>Render method</h3>
-
-        <div>
-            <input
-                type="radio"
-                name="render-method-type"
-                bind:group={simulationState.renderMethodType}
-                value={GpuRenderMethodType.Points}
-                id="render-method-type_points"
-            />
-            <label for="render-method-type_points">Points</label>
-        </div>
-
-        <div>
-            <input
-                type="radio"
-                name="render-method-type"
-                bind:group={simulationState.renderMethodType}
-                value={GpuRenderMethodType.Raymarch}
-                id="render-method-type_raymarch"
-            />
-            <label for="render-method-type_raymarch">Raymarch</label>
-        </div>
-
-        <Separator />
-
-        <h3>Elapsed time</h3>
-
-        <dl>
-            <dt>Time spent in GPU (sample)</dt>
-            <dd>
-                <ElapsedTimeDisplay
-                    ns={simulationState.elapsedTime.gpuTimeNs}
-                    inverseLabel="commands / s"
-                />
-            </dd>
-
-            <dt>Total animation frame time</dt>
-            <dd>
-                <ElapsedTimeDisplay
-                    ns={simulationState.elapsedTime.animationFrameTimeNs}
-                    showMsFractionalPart={false}
-                />
-            </dd>
-        </dl>
-
-        <Separator />
-
-        <h3>Simulation controls</h3>
-
-        <Hotkey
-            key="r"
-            onKeyUp={() => simulationState.reset()}
-        >
-            {#snippet pressTarget({keyHeld})}
-                <Button
-                    {keyHeld}
-                    onclick={() => simulationState.reset()}
-                >Reset (R)</Button>
-            {/snippet}
-        </Hotkey>
-    </overlays-panel>
+    <SimulationStatusPanel
+        {simulationState}
+        {status}
+        {err}
+    />
 </main>
 
 
@@ -113,19 +46,5 @@ main {
     > :global(*) {
         grid-area: 1/1;
     }
-}
-
-overlays-panel {
-    width: 20rem;
-    padding: 0.5rem;
-
-    line-height: 1.25;
-
-    color: oklch(1 0 0);
-}
-
-h3 {
-    font-size: 1.5rem;
-    margin-bottom: 1rem;
 }
 </style>
