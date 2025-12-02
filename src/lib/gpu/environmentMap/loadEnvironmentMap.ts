29197a7bdc026cdc0733b170b9109492f3873884
wooningeire <22846982+wooningeire@users.noreply.github.com>
2025-12-02 17:28:32 -0500

    feat: swap environment

--- src/lib/gpu/environmentMap/loadEnvironmentMap.ts
+++ src/lib/gpu/environmentMap/loadEnvironmentMap.ts
@@ -1,4 +1,4 @@
-import environmentMapUrl from "$lib/assets/qwantani_sunset_puresky_2k.png?url";
+import environmentMapUrl from "$lib/assets/qwantani_moon_noon_puresky_2k.png?url";
 // import { HDRLoader } from "three/addons/loaders/HDRLoader.js";
 
 export const loadEnvironmentMap = async () => {
